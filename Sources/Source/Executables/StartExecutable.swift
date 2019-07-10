a8102a17b931bb20e8bdf54b2ebdd8345a5762b1
Kevin Chen <kevchen2@gmail.com>
2019-07-09 20:28:23 -0700

    Remove post PR argument as it is now broken into an executable

--- Sources/Source/Executables/StartExecutable.swift
+++ Sources/Source/Executables/StartExecutable.swift
@@ -30,7 +30,6 @@ struct StartExecutable: Executable, SlackMessageDeliverable {
         let versionArgument = VersionArgument()
         let previousVersionArgument = PreviousVersionArgument()
         let directoryArgument = DirectoryArgument()
-        let postPRArgument = PostPRArgument()
         let jenkinsOffArgument = JenkinsOffArgument()
         let prettyArgument = PrettyArgument()
         let noInputArgument = NoInputArgument()
@@ -41,7 +40,6 @@ struct StartExecutable: Executable, SlackMessageDeliverable {
             [VersionArgument.argumentName: versionArgument,
              PreviousVersionArgument.argumentName: previousVersionArgument,
              DirectoryArgument.argumentName: directoryArgument,
-             PostPRArgument.argumentName: postPRArgument,
              JenkinsOffArgument.argumentName: jenkinsOffArgument,
              PrettyArgument.argumentName: prettyArgument,
              NoInputArgument.argumentName: noInputArgument,
@@ -94,12 +92,7 @@ struct StartExecutable: Executable, SlackMessageDeliverable {
                 Darwin.exit(1)
             }
         }
-        
-        var runPostOnly = false
-        if let _: PostPRArgument = argumentParser.retrieveArgument() {
-            runPostOnly = true
-        }
-        
+
         var shouldRunJenkins = true
         if let _: JenkinsOffArgument = argumentParser.retrieveArgument() {
             
@@ -208,7 +201,6 @@ struct StartExecutable: Executable, SlackMessageDeliverable {
             Working Directory: \(currentDirectory)
             Target Version: \(version),
             Previous Version: \(previousReleaseBranch?.version ?? "nil")
-            Run Post PR Only: \(runPostOnly)
             Will Run Jenkins: \(shouldRunJenkins)
             Run xcPretty: \(runPretty)
             Will Redirect To Bitbucket: \(shouldRedirectToBitbucket)
@@ -238,132 +230,129 @@ struct StartExecutable: Executable, SlackMessageDeliverable {
         
         // MARK: - Merge branches to Team Merge
         
-        if !runPostOnly {
-            
-            // MARK: Checkout Team Merge Branch
-            Console.writeMessage("**Checking Out \(teamMergeBranch.path) Branch...")
-            let checkOutCommand = GitCommand(arguments: [.checkout(branch: teamMergeBranch.path)])
-            CommandHelper.runCommand(checkOutCommand)
-            
-            // MARK: Pull Team Merge Self
-            Console.writeMessage("**Pulling \(teamMergeBranch.path)...")
-            let pullCommand = GitCommand(arguments: [.pull(branch: nil)])
+        // MARK: Checkout Team Merge Branch
+        Console.writeMessage("**Checking Out \(teamMergeBranch.path) Branch...")
+        let checkOutCommand = GitCommand(arguments: [.checkout(branch: teamMergeBranch.path)])
+        CommandHelper.runCommand(checkOutCommand)
+        
+        // MARK: Pull Team Merge Self
+        Console.writeMessage("**Pulling \(teamMergeBranch.path)...")
+        let pullCommand = GitCommand(arguments: [.pull(branch: nil)])
+        CommandHelper.runCommand(pullCommand)
+        
+        // MARK: Pull From Previous Version
+        if let previousReleaseBranch = previousReleaseBranch {
+            Console.writeMessage("**Pulling from \(previousReleaseBranch.path)...")
+            let pullCommand = GitCommand(arguments: [.pull(branch: previousReleaseBranch.path)])
             CommandHelper.runCommand(pullCommand)
-            
-            // MARK: Pull From Previous Version
-            if let previousReleaseBranch = previousReleaseBranch {
-                Console.writeMessage("**Pulling from \(previousReleaseBranch.path)...")
-                let pullCommand = GitCommand(arguments: [.pull(branch: previousReleaseBranch.path)])
-                CommandHelper.runCommand(pullCommand)
-            }
-            
-            // MARK: Pull all branches into TM branch
-            for branch in developBranchesToRun {
-                
-                Console.writeMessage("**Pulling from \(branch.path) into \(teamMergeBranch.path)...")
-                
-                let pullCommand = GitCommand(arguments: [.pull(branch: branch.path)])
-                pullCommand.run() { result in
-                    switch result {
-                    case .success(let message):
-                        Console.writeMessage("Command Ran Successfully - " + message + "\n", styled: .blue)
-                    case .failure(let error):
-                        let message = "Error: \(error) \n" + " **THERE MIGHT BE A MERGE CONFLICT FROM BRANCH: \(branch.path), please fix if there is one and run script again** \n"
-                        self.exit(1, with: message)
-                    }
-                }
-            }
-            
-            // MARK: Xcode build
-            
-            // Remove pod lock first
-            Console.writeMessage("**Removing PodFile...")
-            let removePodLockCommand = AnyCommand(commandString: "rm", arguments: ["-rf", "Podfile.lock"])
-            CommandHelper.runAndPrintCommand(removePodLockCommand)
-            
-            // Install new pod file
-            Console.writeMessage("**Installing CocoaPods...")
-            let podInstallCommand = AnyCommand(commandString: "pod", arguments: ["install"])
-            let podUpdateCommand = AnyCommand(commandString: "pod", arguments: ["update"])
-            
-            CommandHelper.runAndPrintCommand(podInstallCommand) { result in
-                switch result {
-                case .success:
-                    Console.writeMessage("Command Ran Successfully! \n", styled: .blue)
-                case .failure:
-                    CommandHelper.runAndPrintCommand(podUpdateCommand)
-                }
-            }
-            
-            // Build all targets
-            let buildExecutable = BuildExecutable()
-            buildExecutable.run(targetsToRun: targetsToRun, runPretty: runPretty)
-            
-            // MARK: Push Team Merge
-            Console.writeMessage("**Pushing \(teamMergeBranch.path)...")
-            let pushTMCommand = GitCommand(arguments: [.push])
-            CommandHelper.runCommand(pushTMCommand)
-            
-            // MARK: Check Diff for team merge -> release
-            Console.writeMessage("**Checking Diff, please merge into release...")
-            
-            let runner = SwiftScriptRunner()
-            runner.lock()
-            
-            var count = 0
-            
-            // Check diff between release and team merge to see if it has been merged in yet
-            DiffChecker(branch1: teamMergeBranch.remotePath,
-                        branch2: releaseBranch.remotePath,
-                        restarting: { message, timeTillCheck in
-                            
-                            count += 1
-                            Console.writeMessage(message + "\n" + "** Attempt number: \(count). Still havn't merged, please merge into release, checking again in \(timeTillCheck) sec...", styled: .purple)
-                            
-            },
-                        finished: { success, error in
-                            
-                            if success {
-                                Console.writeMessage("**Merged! Lets continue!", styled: .blue)
-                                runner.unlock()
-                            } else {
-                                var message = ""
-                                if let error = error {
-                                    message = "Error: \(error)"
-                                }
-                                self.exit(1, with: message)
-                            }
-            })
-            
-            if shouldRedirectToBitbucket {
-                Console.writeMessage("**Opening bitbucket PR creation...")
-                let controller = BitbucketController()
-                
-                do {
-                    try controller.redirectToBitbucket(sourceBranch: teamMergeBranch.path, targetBranch: releaseBranch.path)
-                } catch {
-                    Console.writeMessage(error)
-                }
-            }
-            
-            // MARK: Notify slack for PR
-            
-            Console.writeMessage("**Notifing Slack...")
-            
-            let parameters = BitbucketController.parameters(for: teamMergeBranch.path, targetBranch: releaseBranch.path)
-
-            slackController.postPRMessage(bitbucketParameters: parameters) { result in
-                
-                switch result {
-                case .success:
-                    Console.writeMessage("Successfully notified slack! \n", styled: .blue)
-                case .failure(let error):
-                    Console.writeWarning(error)
-                }
-            }
-            
-            runner.wait()
         }
+        
+        // MARK: Pull all branches into TM branch
+        for branch in developBranchesToRun {
+            
+            Console.writeMessage("**Pulling from \(branch.path) into \(teamMergeBranch.path)...")
+            
+            let pullCommand = GitCommand(arguments: [.pull(branch: branch.path)])
+            pullCommand.run() { result in
+                switch result {
+                case .success(let message):
+                    Console.writeMessage("Command Ran Successfully - " + message + "\n", styled: .blue)
+                case .failure(let error):
+                    let message = "Error: \(error) \n" + " **THERE MIGHT BE A MERGE CONFLICT FROM BRANCH: \(branch.path), please fix if there is one and run script again** \n"
+                    self.exit(1, with: message)
+                }
+            }
+        }
+        
+        // MARK: Xcode build
+        
+        // Remove pod lock first
+        Console.writeMessage("**Removing PodFile...")
+        let removePodLockCommand = AnyCommand(commandString: "rm", arguments: ["-rf", "Podfile.lock"])
+        CommandHelper.runAndPrintCommand(removePodLockCommand)
+        
+        // Install new pod file
+        Console.writeMessage("**Installing CocoaPods...")
+        let podInstallCommand = AnyCommand(commandString: "pod", arguments: ["install"])
+        let podUpdateCommand = AnyCommand(commandString: "pod", arguments: ["update"])
+        
+        CommandHelper.runAndPrintCommand(podInstallCommand) { result in
+            switch result {
+            case .success:
+                Console.writeMessage("Command Ran Successfully! \n", styled: .blue)
+            case .failure:
+                CommandHelper.runAndPrintCommand(podUpdateCommand)
+            }
+        }
+        
+        // Build all targets
+        let buildExecutable = BuildExecutable()
+        buildExecutable.run(targetsToRun: targetsToRun, runPretty: runPretty)
+        
+        // MARK: Push Team Merge
+        Console.writeMessage("**Pushing \(teamMergeBranch.path)...")
+        let pushTMCommand = GitCommand(arguments: [.push])
+        CommandHelper.runCommand(pushTMCommand)
+        
+        // MARK: Check Diff for team merge -> release
+        Console.writeMessage("**Checking Diff, please merge into release...")
+        
+        let runner = SwiftScriptRunner()
+        runner.lock()
+        
+        var count = 0
+        
+        // Check diff between release and team merge to see if it has been merged in yet
+        DiffChecker(branch1: teamMergeBranch.remotePath,
+                    branch2: releaseBranch.remotePath,
+                    restarting: { message, timeTillCheck in
+                        
+                        count += 1
+                        Console.writeMessage(message + "\n" + "** Attempt number: \(count). Still havn't merged, please merge into release, checking again in \(timeTillCheck) sec...", styled: .purple)
+                        
+        },
+                    finished: { success, error in
+                        
+                        if success {
+                            Console.writeMessage("**Merged! Lets continue!", styled: .blue)
+                            runner.unlock()
+                        } else {
+                            var message = ""
+                            if let error = error {
+                                message = "Error: \(error)"
+                            }
+                            self.exit(1, with: message)
+                        }
+        })
+        
+        if shouldRedirectToBitbucket {
+            Console.writeMessage("**Opening bitbucket PR creation...")
+            let controller = BitbucketController()
+            
+            do {
+                try controller.redirectToBitbucket(sourceBranch: teamMergeBranch.path, targetBranch: releaseBranch.path)
+            } catch {
+                Console.writeMessage(error)
+            }
+        }
+        
+        // MARK: Notify slack for PR
+        
+        Console.writeMessage("**Notifing Slack...")
+        
+        let parameters = BitbucketController.parameters(for: teamMergeBranch.path, targetBranch: releaseBranch.path)
+        
+        slackController.postPRMessage(bitbucketParameters: parameters) { result in
+            
+            switch result {
+            case .success:
+                Console.writeMessage("Successfully notified slack! \n", styled: .blue)
+            case .failure(let error):
+                Console.writeWarning(error)
+            }
+        }
+        
+        runner.wait()
         
         // MARK: - Post-PR
         let postPRExecutable = PostPRExecutable()
